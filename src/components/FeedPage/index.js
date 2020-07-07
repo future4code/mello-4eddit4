123ee99eb589f3747395a9c61c0dff882a7fae2f
gaspar-d <ghostlabz@gmail.com>
2020-07-07 18:59:01 -0300

    Added Signin+Login+blockAccess+Token

--- src/components/FeedPage/index.js
+++ src/components/FeedPage/index.js
@@ -1,30 +1,53 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { Container, CardPost } from "../GlobalStyles/styles";
 
-export default function index() {
-  return (
-    <Container>
-      <h1>FEED PAGE</h1>
-      <label>Crie seu post:</label>
-      <input placeholder="Escreva seu post" />
-      <Link to="/">
-        <button>Voltar para login</button>
-      </Link>
-      <CardPost>
-        <Link to="/post">
-          <p>Nome de usuário</p>
-          <div>
-            <p>Texto do post</p>
-          </div>
-        </Link>
-        <div>
-          <button>I</button>
-          <span>Counter </span>
-          <button>I</button>
-          <span>comentários</span>
-        </div>
-      </CardPost>
-    </Container>
-  );
+import { useBlockAccess } from "../Hooks/useBlockAccess";
+
+export default function FeedPage() {
+    const history = useHistory();
+    const handleClickLogout = () => {
+        window.localStorage.clear();
+        history.push("/");
+    };
+
+    useBlockAccess();
+    if ("token") {
+        return (
+            <Container>
+                <h1>FEED PAGE</h1>
+                <h3>
+                    Você está logado como:{" "}
+                    {window.localStorage.getItem("username")}
+                </h3>
+                <label>Crie seu post:</label>
+                <button onClick={handleClickLogout}>Logout</button>
+                <input placeholder="Escreva seu post" />
+                <Link to="/">
+                    <button>Voltar para login</button>
+                </Link>
+                <CardPost>
+                    <Link to="/post">
+                        <p>Nome de usuário</p>
+                        <div>
+                            <p>Texto do post</p>
+                        </div>
+                    </Link>
+                    <div>
+                        <button>I</button>
+                        <span>Counter </span>
+                        <button>I</button>
+                        <span>comentários</span>
+                    </div>
+                </CardPost>
+            </Container>
+        );
+    } else {
+        return (
+            <div>
+                <p>Error</p>
+            </div>
+        );
+        //<Link to="/error" />;
+    }
 }
